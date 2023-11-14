81b885db6be49dc3ae166793d0954cce51731283
Marcos923 <marcos.daut@gmail.com>
2023-11-14 13:06:46 -0300

    Añadiendo Pacientes al State principal y reiniciando Formularios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,44 @@
 import { useState, useEffect } from "react";
 
-const Formulario = () => {
-  const [nombre, setNombre] = useState("Thor");
+const Formulario = ({ pacientes, setPacientes }) => {
+  const [nombre, setNombre] = useState("");
+  const [propietario, setPropietario] = useState("");
+  const [email, setEmail] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [sintomas, setSintomas] = useState("");
+
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validacion del formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes("")) {
+      console.log("Hay al menos un campo vacio");
+      setError(true);
+      return;
+    }
+    setError(false);
+
+    //Objeto de paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas,
+    };
+
+    //Tomar una copia de lo que hay en pacientes (spreed operation) y pasamos objeto pacientes, devuelve un arreglo nuevo que se asigna inmediatamente a setPacientes
+    setPacientes([...pacientes, objetoPaciente]);
+
+    //Reiniciar el formulario despues de hacer submit
+    setNombre("");
+    setPropietario("");
+    setEmail("");
+    setFecha("");
+    setSintomas("");
+  };
 
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-3">
@@ -12,7 +49,15 @@ const Formulario = () => {
         Añade pacientes y{" "}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
-      <form className="bg-white shadow-md rounded-lg py-10 px-5  mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5  mb-10"
+      >
+        {error && (
+          <div className="bg-red-800 text-center text-white rounded-md p-3 mb-3 font-bold uppercase">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        )}
         <div className="mb-5">
           <label
             htmlFor="mascota"
@@ -41,6 +86,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre del propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -55,6 +102,8 @@ const Formulario = () => {
             type="email"
             placeholder="Email contacto"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -68,6 +117,8 @@ const Formulario = () => {
             id="alta"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -81,6 +132,8 @@ const Formulario = () => {
             id="sintomas"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Describe los Sintomas"
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
           />
         </div>
 
