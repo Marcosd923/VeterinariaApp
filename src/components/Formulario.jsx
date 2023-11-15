473b925c3167635fb8cc83631e2d5344ac4cc488
Marcos923 <marcos.daut@gmail.com>
2023-11-14 21:08:55 -0300

    mostrar textos de listadoPacientes solo cuando se cargue un paciente y agregando botones de editar y eliminar pacientes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -144,7 +144,7 @@ const Formulario = ({ pacientes, setPacientes }) => {
 
         <input
           type="submit"
-          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all rounded-md"
           value="agregar paciente"
         />
       </form>
